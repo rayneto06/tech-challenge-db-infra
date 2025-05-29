d8337ee3e58a4fb86bd6922f210d556a41dbd186
Raymundo Neto <raymundocneto@gmail.com>
2025-05-29 11:01:09 -0300

    refactor

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "customers_table_name" {
-  description = "DynamoDB table name for Customers"
+  description = "Name of the Customers table"
   value       = aws_dynamodb_table.customers.name
 }
 
 output "products_table_name" {
-  description = "DynamoDB table name for Products"
+  description = "Name of the Products table"
   value       = aws_dynamodb_table.products.name
 }
 
 output "orders_table_name" {
-  description = "DynamoDB table name for Orders"
+  description = "Name of the Orders table"
   value       = aws_dynamodb_table.orders.name
 }
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region    = "us-east-1"
+read_capacity = 5
+write_capacity= 5
